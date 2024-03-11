abdc49ba1719a19fb161f0fa84108055d549ba16
Stephen Rasku <stephen.rasku@non.se.com>
2024-03-11 14:05:35 -0700

    Add vim-tmux-navigator plugin
    Configured to use Alt-h, -j, -k, and -l keys.

---
+++ lua/custom/plugins/tmux-navigator.lua
@@ -0,0 +1,18 @@
+return {
+  'christoomey/vim-tmux-navigator',
+  cmd = {
+    'TmuxNavigateLeft',
+    'TmuxNavigateDown',
+    'TmuxNavigateUp',
+    'TmuxNavigateRight',
+    'TmuxNavigatePrevious',
+  },
+  keys = {
+    { '<m-h>', '<cmd>TmuxNavigateLeft<cr>' },
+    { '<m-j>', '<cmd>TmuxNavigateDown<cr>' },
+    { '<m-k>', '<cmd>TmuxNavigateUp<cr>' },
+    { '<m-l>', '<cmd>TmuxNavigateRight<cr>' },
+    { '<m-\\>', '<cmd>TmuxNavigatePrevious<cr>' },
+  },
+  lazy = false,
+}
