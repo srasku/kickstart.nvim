189a43a370c178cafbe28c54af7467aedfefe7b3
Stephen Rasku <stephen.rasku@non.se.com>
2024-03-13 09:16:40 -0700

    Default to vertical diffs.

--- lua/options.lua
+++ lua/options.lua
@@ -56,4 +56,7 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- Who wants horizontal diffs?
+vim.opt.diffopt:append 'vertical'
+
 -- vim: ts=2 sts=2 sw=2 et
