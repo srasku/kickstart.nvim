50b58e5e9d1a34191129861c5d509a1eb53460d8
Stephen Rasku <mercurial@srasku.net>
2024-04-10 17:11:40 -0700

    Use 'FileType' inside 'gitcommit' autocmd
    This is a tiny bit more concise.

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -5,12 +5,11 @@ local autocmd = vim.api.nvim_create_autocmd
 autocmd('BufWritePre', { pattern = '', command = ':%s/\\s\\+$//e' })
 
 -- Enable spell checking for Git commit messages
-autocmd('BufEnter', {
+autocmd('FileType', {
   desc = 'Enable spell checking on Git commit messages',
+  pattern = 'gitcommit',
   group = augroup('git_spell', { clear = true }),
   callback = function(opts)
-    if vim.bo[opts.buf].filetype == 'gitcommit' then
-      vim.cmd 'set spell'
-    end
+    vim.cmd 'set spell'
   end,
 })
