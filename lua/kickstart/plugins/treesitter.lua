a1ce4adae255d692da7a25fd687670dd9cbd1715
Stephen Rasku <mercurial@srasku.net>
2024-06-30 12:44:30 -0700

    Enable incremental selection for Tree-sitter
    It is using `ctrl-Space` for this purpose but it might interact badly
    with `tmux` since this is also the prefix key that I use for `tmux`.  If
    it ends up being a problem I will update this to something else.

--- lua/kickstart/plugins/treesitter.lua
+++ lua/kickstart/plugins/treesitter.lua
@@ -12,12 +12,19 @@ return {
         auto_install = true,
         highlight = { enable = true },
         indent = { enable = true },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = '<C-space>',
+            node_incremental = '<C-space>',
+            node_decremental = '<bs>',
+          },
+        },
       }
 
       -- There are additional nvim-treesitter modules that you can use to interact
       -- with nvim-treesitter. You should go explore a few and see what interests you:
       --
-      --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
       --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
       --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
     end,
