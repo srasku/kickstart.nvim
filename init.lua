c21abbfe8492732c8a95024922b330baf708251c
Stephen Rasku <mercurial@srasku.net>
2024-09-22 15:06:23 -0700

    Update MiniStatusLine* colours
    So I can see the inactive status line.

--- init.lua
+++ init.lua
@@ -12,5 +12,6 @@ require 'keymaps'
 require 'lazy-bootstrap'
 require 'lazy-plugins'
 require 'autocmds'
+require 'colours'
 
 -- vim: ts=2 sts=2 sw=2 et
---
+++ lua/colours.lua
@@ -0,0 +1,2 @@
+vim.api.nvim_set_hl(0, 'MiniStatusLineFilename', { bg = '#7aa2f7', fg = '#16161e' })
+vim.api.nvim_set_hl(0, 'MiniStatusLineInactive', { bg = '#3b4261', fg = '#a9b1dc' })
