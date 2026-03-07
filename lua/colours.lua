f1f18cae872b44df765f17593cd3930edf10b3d8
Stephen Rasku <mercurial@srasku.net>
2026-03-07 10:09:18 -0800

    Use green for 'DiffAdd' highlight group.

--- lua/colours.lua
+++ lua/colours.lua
@@ -1,2 +1,3 @@
 vim.api.nvim_set_hl(0, 'MiniStatusLineFilename', { bg = '#7aa2f7', fg = '#16161e' })
 vim.api.nvim_set_hl(0, 'MiniStatusLineInactive', { bg = '#3b4261', fg = '#a9b1dc' })
+vim.api.nvim_set_hl(0, 'DiffAdd', { bg = 'bg', fg = '#899823' })
